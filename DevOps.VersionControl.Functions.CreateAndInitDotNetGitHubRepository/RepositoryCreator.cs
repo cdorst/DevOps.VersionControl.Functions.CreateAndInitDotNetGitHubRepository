90dae5a47a41113398b166971b893c845f98db09
Christopher Dorst <chris@dorst.email>
2018-01-03 23:56:36 -0600

    Moving GetRemote responsibility into GetGitHubRepoRemoteUri

--- DevOps.VersionControl.Functions.CreateAndInitDotNetGitHubRepository/RepositoryCreator.cs
+++ DevOps.VersionControl.Functions.CreateAndInitDotNetGitHubRepository/RepositoryCreator.cs
@@ -1,25 +1,18 @@
 ﻿using Common.Structures.HttpBasicAuthentication;
 using DevOps.VersionControl.Structures.GitCommitUser;
-using System;
 using System.Threading.Tasks;
 using static DevOps.VersionControl.Functions.CreateGitHubRepository.GitHubRepositoryCreator;
+using static DevOps.VersionControl.Functions.GetGitHubRepoRemoteUri.RemoteUriGetter;
 using static DevOps.VersionControl.Functions.InitializeDotNetGnuGitRepository.GitRepositoryInitializer;
-using static Metaproject.Users.CDorst.GitHubAccessToken.GitHubCredentials;
 
 namespace DevOps.VersionControl.Functions.CreateAndInitDotNetGitHubRepository
 {
     public static class RepositoryCreator
     {
-        private const string RemoteRoot = "https://github.com/";
-
         public static async Task CreateRepository(string directory, string name, string description, BasicAuthenticationCredentials? credentials = null, UserInfo? user = null)
         {
-            credentials = credentials ?? CDorst;
             await Create(name, description, credentials);
-            InitializeRepository(directory, GetRemote(credentials.Value.User, name), user);
+            InitializeRepository(directory, RemoteUri(name, credentials), user);
         }
-
-        private static Uri GetRemote(string user, string name)
-            => new Uri($"{RemoteRoot}{user}/{name}.git");
     }
 }
